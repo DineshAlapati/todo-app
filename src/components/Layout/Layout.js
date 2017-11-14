ff0869c2e8b1a4ef4daafe3ba64bf29afab15957
Dinesh Alapati <dine.alapati@gmail.com>
2017-11-13 22:26:52 -0500

    Remove Header component in Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Layout.css';
-import Header from '../Header/Header';
 
 class Layout extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
   };
   render() {
-    return (
-      <div className="App">
-        <Header />
-        {this.props.children}
-      </div>
-    );
+    return <div className="App">{this.props.children}</div>;
   }
 }
 
